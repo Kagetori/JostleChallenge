df3447ca3f10315d179edd7ee3019cf07c5198ee
Meng Tian <meng.tian401@gmail.com>
2016-08-30 23:17:05 -0600

    Added video to tab1

--- src/com/example/jostlechallenge/client/JostleChallenge.java
+++ src/com/example/jostlechallenge/client/JostleChallenge.java
@@ -2,10 +2,12 @@ package com.example.jostlechallenge.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.VideoElement;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.jsonp.client.JsonpRequestBuilder;
+import com.google.gwt.media.client.Video;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.DeckPanel;
@@ -67,6 +69,19 @@ public class JostleChallenge implements EntryPoint {
 		getTitleAndBody();
 		tabOne.add(tabOneTitle);
 		tabOne.add(tabOneBody);
+		
+		Video video = Video.createIfSupported();
+		if (video == null) {
+			//Error message here
+			Window.alert("HTML5 Video is not supported by your browser");
+			return;
+		}
+		video.addSource("http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4", VideoElement.TYPE_MP4);
+		video.setControls(true);
+		//video.setHeight("500px");
+		//video.setSize("500px", "500px");
+		video.setStyleName("tabOneVideo");
+		tabOne.add(video);
 	}
 
 	private void getTitleAndBody() {
