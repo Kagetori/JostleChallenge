6c7a241f633596356a633bef8c692155a6dc7077
Meng Tian <meng.tian401@gmail.com>
2016-08-30 19:04:17 -0600

    Made tabs at bottom

--- src/com/example/jostlechallenge/client/JostleChallenge.java
+++ src/com/example/jostlechallenge/client/JostleChallenge.java
@@ -2,9 +2,14 @@ package com.example.jostlechallenge.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TabLayoutPanel;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.ui.DeckPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.TabBar;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -15,15 +20,35 @@ public class JostleChallenge implements EntryPoint {
 	 * Entry point method.
 	 */
 	public void onModuleLoad() {
-		// TODO Assemble tab panel
-		TabLayoutPanel mainPanel = new TabLayoutPanel(50, Unit.PX); // height of
-																	// tab bar
-		mainPanel.add(new HTML("this"), "[this]");
-		mainPanel.add(new HTML("that"), "[that]");
-		mainPanel.add(new HTML("the other"), "[the other]");
+		TabBar tabs = new TabBar();
+		final DeckPanel deck = new DeckPanel(); 
+
+		for(int i = 0; i < 3; i++) {
+		    tabs.addTab("Tab " + Integer.toString(i + 1));
+		    deck.add(new Label("Label " + Integer.toString(i + 1)));
+		}
+
+		tabs.addSelectionHandler(new SelectionHandler<Integer>() {
+		    @Override
+		    public void onSelection(SelectionEvent<Integer> event) {
+		        deck.showWidget(event.getSelectedItem());
+		    }
+		});
+
+		VerticalPanel vPanel = new VerticalPanel();
+		vPanel.add(deck);
+		vPanel.add(tabs);
 		
-		//TODO Associate main panel with HTML
-		RootPanel.get("jostleChallenge").add(mainPanel);
+		tabs.selectTab(0);
+		
+		LayoutPanel p = new LayoutPanel();
+		p.add(deck);
+		p.add(tabs);
+		
+		p.setWidgetBottomHeight(tabs, 0, Unit.PX, 50, Unit.PX);	
+		
+//		//Associate main panel with HTML
+		RootLayoutPanel.get().add(p);
 		
 
 		// TODO Create 1st tab
