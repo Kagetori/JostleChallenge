3a2d63cc7a853dd392c4eef68e33554f86779a49
Meng Tian <meng.tian401@gmail.com>
2016-09-04 18:30:10 -0600

    Deleted Greeting Service

--- src/com/example/jostlechallenge/server/JsonServiceImpl.java
+++ src/com/example/jostlechallenge/server/JsonServiceImpl.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import com.example.jostlechallenge.client.Picture;
 import com.example.jostlechallenge.client.JsonService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
