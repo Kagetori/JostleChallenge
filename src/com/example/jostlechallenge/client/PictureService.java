a8aa53d626c3b9b1f608cef8c15fcd8ac3f13b7c
Meng Tian <meng.tian401@gmail.com>
2016-09-04 18:33:15 -0600

    Remove deleted classes

--- src/com/example/jostlechallenge/client/PictureService.java
+++
@@ -1,9 +0,0 @@
-package com.example.jostlechallenge.client;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-@RemoteServiceRelativePath("pictures")
-public interface PictureService extends RemoteService {
-	Picture getPictures();
-}
--- src/com/example/jostlechallenge/client/PictureServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.example.jostlechallenge.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface PictureServiceAsync {
-
-	void getPictures(AsyncCallback<Picture> callback);
-
-}
